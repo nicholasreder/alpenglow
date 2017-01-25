1c5a767edf34d2f3d7c7891ca77699416fcb9da7
Ariel Rokem <arokem@gmail.com>
2017-01-25 20:46:55 +0000

    Break down stitching into two stages

--- alpenglow/stitching.py
+++ alpenglow/stitching.py
@@ -15,6 +15,24 @@ def stitch(image1, image2):
     -------
     Stitched 2D numpy array
     """
+    shift = find_shift(image1, image2)
+    registered = apply_shift(image1, image2, shift)
+    return registered, shift
+
+
+def find_shift(image1, image2):
+    """ 
+    Identify lateral shifts to stitch two images
+    
+    Parameters
+    ----------
+    image1, image2 : 2D arrays
+        The images to be stitched, *in order* (bottom, top)
+
+    Returns
+    -------
+    tuple : lateral shifts (x, y)
+    """
     
     cols1 = image1.shape[1]
     cols2 = image2.shape[1]
@@ -22,9 +40,34 @@ def stitch(image1, image2):
     rows2 = image2.shape[0]
     shift, error, diffphase = register_translation(image1[:rows1//2, int(cols1//2-0.1*cols1):int(cols1//2+0.1*cols1)],
                                                    image2[rows2//2:, int(cols2//2-0.1*cols2):int(cols2//2+0.1*cols2)])
+    return shift
+
+
+def apply_shift(image1, image2, shift):
+    """ 
+    Apply a lateral shift between two images, stitching them together
+    
+    Parameters
+    ----------
+    image1, image2 : 2D arrays
+        The images to be stitched, *in order* (bottom, top)
+    
+    shift : sequence of length 2 
+        x, y lateral shifts
+    
+    Returns
+    -------
+    Stitched image
+    
+    """
+    
+    cols1 = image1.shape[1]
+    cols2 = image2.shape[1]
+    rows1 = image1.shape[0]
+    rows2 = image2.shape[0]
 
     overlap=rows1 // 2 + shift[0]
-    registered = np.zeros((rows1 + rows2 - overlap, cols1))
+    registered = np.zeros((rows1 + rows2 - overlap, cols1), dtype=int)
     registered[:rows2] = image2
     registered[rows2:, :cols1-shift[1]] = image1[overlap:, shift[1]:] 
-    return registered, shift
+    return registered
