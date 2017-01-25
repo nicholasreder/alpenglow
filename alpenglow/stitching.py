c6eca17be19e9223a48abb0aa4804b9372e57b28
Nicholas Reder <nreder@uw.edu>
2017-01-25 21:26:14 +0000

    fixed for negative shifts

--- alpenglow/stitching.py
+++ alpenglow/stitching.py
@@ -38,7 +38,7 @@ def find_shift(image1, image2):
     cols2 = image2.shape[1]
     rows1 = image1.shape[0]
     rows2 = image2.shape[0]
-    shift, error, diffphase = register_translation(image1[:rows1//2, int(cols1//2-0.1*cols1):int(cols1//2+0.1*cols1)],
+    shift, error, diffphase = register_translation(image1[:rows2//2, int(cols2//2-0.1*cols2):int(cols2//2+0.1*cols2)],
                                                    image2[rows2//2:, int(cols2//2-0.1*cols2):int(cols2//2+0.1*cols2)])
     return shift
 
@@ -69,5 +69,8 @@ def apply_shift(image1, image2, shift):
     overlap=rows1 // 2 + shift[0]
     registered = np.zeros((rows1 + rows2 - overlap, cols1), dtype=int)
     registered[:rows2] = image2
-    registered[rows2:, :cols1-shift[1]] = image1[overlap:, shift[1]:] 
+    if shift[1] > 0:
+        registered[rows2:, :cols1-shift[1]] = image1[overlap:, shift[1]:] 
+    else:
+        registered[rows2:, abs(shift[1]):] = image1[overlap:, :shift[1]] 
     return registered
