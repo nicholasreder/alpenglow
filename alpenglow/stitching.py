1551f1b6a2fd0a10e103626a434b972358a9f759
Nicholas Reder <nreder@uw.edu>
2017-02-07 21:09:51 +0000

    added z-stack function to stitching

--- alpenglow/stitching.py
+++ alpenglow/stitching.py
@@ -1,6 +1,7 @@
 from skimage import transform
 import numpy as np
 from skimage.feature import register_translation
+from skimage.io import imread_collection, ImageCollection
 
 def stitch(image1, image2):
     """
@@ -85,3 +86,44 @@ def apply_shift(image1, image2, shift, margin=100):
         registered[rows2 - margin:rows2] = fade1 + fade2
 
     return registered.astype(int)
+
+def stitch_zstack(images_1, images_2, current_stack):
+    """
+    Stitch two z-stacks of tiff strips
+    
+    Parameters
+    ----------
+    images_1, images_2 objects of type image_collection
+    current_stack is an integer
+    
+    """
+    sub_images_1 = []
+    sub_images_2 = []
+    ps = []
+    for x in range(10,100,10):
+        p = int(np.percentile(np.arange(len(images_1)), x))
+        sub_images_1_p = images_1[p]
+        sub_images_2_p = images_2[p]
+        sub_images_1.append(sub_images_1_p)
+        sub_images_2.append(sub_images_2_p)
+        ps.append(p)
+    
+    shift = []
+    for x in range(len(sub_images_1)):
+        shift.append(find_shift(sub_images_1[x], sub_images_2[x]))
+    shift = np.array(shift)
+    coef = []
+    coef = np.polyfit(ps, shift[:,0], 1)
+    
+    shift_last = int(np.round(np.polyval(coef, len(images_1))))
+    rows = images_1[0].shape[0]+images_2[0].shape[0]
+    overlap = (images_2[0].shape[0]//2)+shift_last
+    chop_index = int(rows - overlap)
+
+    for z_level in range(len(images_1)):
+        shift_zero = int(np.round(np.polyval(coef, z_level)))
+        shift_one = int(np.round(np.mean(shift[:,1])))
+        registered = apply_shift(images_1[z_level], images_2[z_level], [shift_zero, shift_one])
+        tiff.imsave("mosaic_%06d_%06d.tif"%(current_stack, z_level+1), registered[:chop_index]) 
+    return ImageCollection("mosaic_%06d_*.tif"%(current_stack), plugin="tifffile")
+    
