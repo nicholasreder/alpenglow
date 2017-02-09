570a4756f57b33a41206aebe1f2eacf096dd4791
Ariel Rokem <arokem@ip-10-25-29-56.us-west-2.compute.internal>
2017-02-09 00:04:49 +0000

    Stitch with either memmap or ImageCollection

--- alpenglow/stitching.py
+++ alpenglow/stitching.py
@@ -1,7 +1,14 @@
-from skimage import transform
+from tempfile import mkdtemp
+import os.path as op
+
 import numpy as np
+
+import skimage
+from skimage import transform
 from skimage.feature import register_translation
 from skimage.io import imread_collection, ImageCollection
+import skimage.external.tifffile as tiff
+
 
 def stitch(image1, image2):
     """
@@ -87,43 +94,70 @@ def apply_shift(image1, image2, shift, margin=100):
 
     return registered.astype(int)
 
-def stitch_zstack(images_1, images_2, current_stack):
+
+def stitch_zstack(images_1, images_2, current_stack=None, out_path=None):
     """
     Stitch two z-stacks of tiff strips
     
     Parameters
     ----------
-    images_1, images_2 objects of type image_collection
-    current_stack is an integer
-    
+    images_1, images_2 : objects of type ImageCollection, or numpy mem-mapped arrays
+
+    current_stack : int, optional
+        The serial number of the current stack that is being stitched. This is only
+        consequential in the case in which tiffs are saved (will be saved as 
+        "mosaic_%06d_%06d.tif"%(current_stack, z_level+1))
+
+    out_path : str
+        Where to save output files (only relevant if input are ImageCollection 
+        objects)
+
+    Returns 
+    -------
+    Depending on the input: ImageCollection or numpy mem-mapped array
     """
-    sub_images_1 = []
-    sub_images_2 = []
-    ps = []
-    for x in range(10,100,10):
-        p = int(np.percentile(np.arange(len(images_1)), x))
-        sub_images_1_p = images_1[p]
-        sub_images_2_p = images_2[p]
-        sub_images_1.append(sub_images_1_p)
-        sub_images_2.append(sub_images_2_p)
-        ps.append(p)
+    percentiles = [int(np.percentile(np.arange(len(images_1)), x)) for x in range(10, 100, 10)]
+    sub_images_1 = np.zeros((len(percentiles), images_1[0].shape[0], images_1[0].shape[1]))
+    sub_images_2 = np.zeros((len(percentiles), images_2[0].shape[0], images_2[0].shape[1]))
+
+    for ii, p in enumerate(percentiles):
+        sub_images_1[ii]= images_1[p]
+        sub_images_2[ii] = images_2[p]
     
     shift = []
     for x in range(len(sub_images_1)):
         shift.append(find_shift(sub_images_1[x], sub_images_2[x]))
     shift = np.array(shift)
     coef = []
-    coef = np.polyfit(ps, shift[:,0], 1)
+    coef = np.polyfit(percentiles, shift[:,0], 1)
     
     shift_last = int(np.round(np.polyval(coef, len(images_1))))
     rows = images_1[0].shape[0]+images_2[0].shape[0]
     overlap = (images_2[0].shape[0]//2)+shift_last
     chop_index = int(rows - overlap)
-
-    for z_level in range(len(images_1)):
-        shift_zero = int(np.round(np.polyval(coef, z_level)))
-        shift_one = int(np.round(np.mean(shift[:,1])))
-        registered = apply_shift(images_1[z_level], images_2[z_level], [shift_zero, shift_one])
-        tiff.imsave("mosaic_%06d_%06d.tif"%(current_stack, z_level+1), registered[:chop_index]) 
-    return ImageCollection("mosaic_%06d_*.tif"%(current_stack), plugin="tifffile")
     
+    if isinstance(images_1, skimage.io.collection.ImageCollection):
+        # Do the relatively slow thing, but save the results
+        if out_path is None:
+            out_path = '.'
+        if current_stack is None:
+            current_stack = 1
+        for z_level in range(len(images_1)):
+            shift_zero = int(np.round(np.polyval(coef, z_level)))
+            shift_one = int(np.round(np.mean(shift[:,1])))
+            registered = apply_shift(images_1[z_level], images_2[z_level], [shift_zero, shift_one])
+            tiff.imsave(op.join(out_path, "mosaic_%06d_%06d.tif"%(current_stack, z_level+1)), 
+                        registered[:chop_index]) 
+        return ImageCollection(op.join(out_path, "mosaic_%06d_*.tif"%(current_stack)),
+                               plugin="tifffile")
+    else:
+        # Do the relatively fast thing, without saving
+        f3 = op.join(mkdtemp(), 'reg.dat')
+        h3 = open(f3, 'w')
+        registered = np.memmap(f3, dtype=images_1.dtype, shape=(len(images_1), chop_index, images_1[0].shape[1]))
+        for z_level in range(len(images_1)):
+            shift_zero = int(np.round(np.polyval(coef, z_level)))
+            shift_one = int(np.round(np.mean(shift[:,1])))
+            registered[z_level] = apply_shift(images_1[z_level], images_2[z_level], [shift_zero, shift_one])[:chop_index]
+
+        return registered
