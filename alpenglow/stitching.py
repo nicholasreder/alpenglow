7767c1beb81e0214d856baf8dd3aad37e0f0e13e
Ariel Rokem <arokem@uw.edu>
2017-03-09 21:46:06 +0000

    Stitching with multi-channel data. Implement default when registration fails (no shift)

--- alpenglow/stitching.py
+++ alpenglow/stitching.py
@@ -1,5 +1,6 @@
 from tempfile import mkdtemp
 import os.path as op
+import warnings
 
 import numpy as np
 
@@ -86,7 +87,7 @@ def apply_shift(image1, image2, shift, margin=100):
     overlap = int(rows2 // 2 + shift[0])
     registered = np.zeros((rows1 + rows2 - overlap, cols1), dtype=int)
     registered[:rows2-margin] = image2[:rows2-margin]
-    if shift[1] > 0:
+    if shift[1] >= 0:
         registered[rows2-margin:, :cols1-int(shift[1])] = image1[overlap-margin:, int(shift[1]):] 
     else:
         registered[rows2-margin:, abs(int(shift[1])):] = image1[overlap-margin:, :int(shift[1])] 
@@ -94,7 +95,7 @@ def apply_shift(image1, image2, shift, margin=100):
     if margin > 0:
         fade2 = image2[rows2 - margin:rows2] * np.arange(1, 0, -0.01)[:, np.newaxis]
         fade1 = np.zeros_like(fade2)
-        if shift[1] > 0:
+        if shift[1] >= 0:
             fade1[:, :cols1-int(shift[1])] = (image1[overlap-margin:overlap, int(shift[1]):] * 
                                               np.arange(0, 1, 0.01)[:, np.newaxis])
         else:
@@ -103,6 +104,7 @@ def apply_shift(image1, image2, shift, margin=100):
         registered[rows2 - margin:rows2] = fade1 + fade2
 
     return registered.astype(int)
+
 
 def test_image(path, first_strip, last_strip, percentile = 50):
     """ 
@@ -130,34 +132,32 @@ def test_image(path, first_strip, last_strip, percentile = 50):
     return mosaic
 
 
-
-def stitch_zstack(images_1, images_2, current_stack=None, out_path=None):
+def calc_coef(images_1, images_2, nchan, chan):
     """
     Stitch two z-stacks of tiff strips
     
     Parameters
     ----------
     images_1, images_2 : objects of type ImageCollection, or numpy mem-mapped arrays
+    
+    nchan : number of channels aquired (int)
 
-    current_stack : int, optional
-        The serial number of the current stack that is being stitched. This is only
-        consequential in the case in which tiffs are saved (will be saved as 
-        "mosaic_%06d_%06d.tif"%(current_stack, z_level+1))
-
-    out_path : str
-        Where to save output files (only relevant if input are ImageCollection 
-        objects)
+    chan : which channel to calculate coefficients (int)
 
     Returns 
     -------
     Depending on the input: ImageCollection or numpy mem-mapped array
     """
-    percentiles = [int(np.percentile(np.arange(len(images_1)), x)) for x in range(10, 100, 10)]
+    if nchan > 1:
+        images_1 = images_1[chan*images_1.shape[0]//nchan:(chan+1)*images_1.shape[0]//nchan,:]
+        images_2 = images_2[chan*images_2.shape[0]//nchan:(chan+1)*images_2.shape[0]//nchan,:]
+    
+    percentiles = [int(np.percentile(np.arange(len(images_1)), x)) for x in range(30, 80, 10)]
     sub_images_1 = np.zeros((len(percentiles), images_1[0].shape[0], images_1[0].shape[1]))
     sub_images_2 = np.zeros((len(percentiles), images_2[0].shape[0], images_2[0].shape[1]))
 
     for ii, p in enumerate(percentiles):
-        sub_images_1[ii]= images_1[p]
+        sub_images_1[ii] = images_1[p]
         sub_images_2[ii] = images_2[p]
     
     shift = []
@@ -166,12 +166,10 @@ def stitch_zstack(images_1, images_2, current_stack=None, out_path=None):
     shift = np.array(shift)
     coef = []
     coef = np.polyfit(percentiles, shift[:,0], 1)
-    
-    shift_last = int(np.round(np.polyval(coef, len(images_1))))
-    rows = images_1[0].shape[0]+images_2[0].shape[0]
-    overlap = (images_2[0].shape[0]//2)+shift_last
-    chop_index = int(rows - overlap)
-    
+    return coef, shift
+
+
+def output_zstack(images_1, images_2, coef, shift, chop_index, chan=0, current_stack=None, out_path=None):
     if isinstance(images_1, skimage.io.collection.ImageCollection):
         # Do the relatively slow thing, but save the results
         if out_path is None:
@@ -182,9 +180,9 @@ def stitch_zstack(images_1, images_2, current_stack=None, out_path=None):
             shift_zero = int(np.round(np.polyval(coef, z_level)))
             shift_one = int(np.round(np.mean(shift[:,1])))
             registered = apply_shift(images_1[z_level], images_2[z_level], [shift_zero, shift_one])
-            tiff.imsave(op.join(out_path, "mosaic_%06d_%06d.tif"%(current_stack, z_level+1)), 
+            tiff.imsave(op.join(out_path, "mosaic_chan%06d_%06d_%06d.tif"%(chan, current_stack, z_level+1)), 
                         registered[:chop_index]) 
-        return ImageCollection(op.join(out_path, "mosaic_%06d_*.tif"%(current_stack)),
+        return ImageCollection(op.join(out_path, "mosaic_chan%06d_%06d_*.tif"%(chan, current_stack)),
                                plugin="tifffile")
     else:
         # Do the relatively fast thing, without saving
@@ -194,6 +192,45 @@ def stitch_zstack(images_1, images_2, current_stack=None, out_path=None):
         for z_level in range(len(images_1)):
             shift_zero = int(np.round(np.polyval(coef, z_level)))
             shift_one = int(np.round(np.mean(shift[:,1])))
-            registered[z_level] = apply_shift(images_1[z_level], images_2[z_level], [shift_zero, shift_one])[:chop_index]
+            registered[z_level] = apply_shift(images_1[z_level], images_2[z_level], 
+                                              [shift_zero, shift_one])[:chop_index]
 
         return registered
+
+
+def stitch_zstack(images_1, images_2, nchan, chan=0, current_stack=None, out_path=None):
+    coef, shift = calc_coef(images_1, images_2, nchan, chan)
+    if np.std(shift[:, 0]) > np.sqrt(len(images_1) // nchan): 
+        warnings.warn("I am having trouble finding a good registration. Defaulting to no shift")
+        shift = np.zeros(shift.shape)
+        coef = np.zeros(coef.shape)
+    shift_last = int(np.round(np.polyval(coef, len(images_1))))
+    rows = images_1[0].shape[0]+images_2[0].shape[0]
+    overlap = (images_2[0].shape[0]//2)+shift_last
+    chop_index = int(rows - overlap)
+    if nchan == 1:
+        return output_zstack(images_1, images_2, coef, shift, chop_index, current_stack=current_stack, out_path=out_path)
+
+    if nchan > 1:       
+        if isinstance(images_1, skimage.io.collection.ImageCollection):
+            im_collection_list = []
+            for chan_index in range(nchan):
+                im_collection_list.append(output_zstack(
+                    images_1[chan_index*images_1.shape[0]//nchan:(chan_index+1)*images_1.shape[0]//nchan,:],
+                    images_2[chan_index*images_2.shape[0]//nchan:(chan_index+1)*images_2.shape[0]//nchan,:], 
+                    coef, shift, chop_index, current_stack=current_stack, out_path=out_path))
+            all_col_files = []
+            for col in im_collection_list:
+                all_col_files.extend(col.files)
+            return imread_collection(all_col_files)    
+        else:
+            f3 = op.join(mkdtemp(), 'reg_nchan.dat')
+            h3 = open(f3, 'w')
+            registered = np.memmap(f3, dtype=images_1.dtype, shape=(nchan, len(images_1)//nchan, 
+                                                                    chop_index, images_1[0].shape[1]))
+            for chan_index in range(nchan):
+                registered[chan_index] = output_zstack(
+                    images_1[chan_index*images_1.shape[0]//nchan:(chan_index+1)*images_1.shape[0]//nchan,:],
+                    images_2[chan_index*images_2.shape[0]//nchan:(chan_index+1)*images_2.shape[0]//nchan,:], 
+                    coef, shift, chop_index, current_stack=current_stack, out_path=out_path)
+            return registered 
